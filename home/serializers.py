eae24afc43b707c678acbd13d8b31e39455edd1f
nikhil <nikhil@nikhils-MacBook-Air.local>
2024-02-26 23:39:07 +0530

    added serializers

---
+++ home/serializers.py
@@ -0,0 +1,16 @@
+from .models import Files,Folder
+from rest_framework import serializers
+
+
+class FileListSerializers(serializers.Serializer):
+    files = serializers.ListField(
+        child = serializers.FileField(max_length = 1000000,allow_empty_file = False,use_url = False)
+    )
+    def create(self, validated_data):
+        folder = Folder.objects.create()
+        files = validated_data.pop('files')
+        files_objs = []
+        for file in files:
+            files_obj = Files.objects.create(folder = folder,file = file)
+            files_objs.append(files_obj)
+        return {'files': {}, 'folder': str(folder.uid   )}
